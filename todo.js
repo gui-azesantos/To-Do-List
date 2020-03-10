a2699ae9c4bf2813e9ca612314bd71790b40f6a2
gui-azesantos <55855695+gui-azesantos@users.noreply.github.com>
2020-03-10 09:31:42 -0300

    Finalizando app Todo

--- todo.js
+++ todo.js
@@ -1,21 +1,49 @@
 var listElement = document.querySelector('#app ul');
-var inputElement = document.querrySelector('#app input');
-var buttonElement = document.querrySelector('#app button');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer Café',
-    'Estudar JavaScript',
-    'Acessar Comunidade'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function Render() {
+    listElement.innerHTML = ''
     for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
 
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick' , 'deleteTodo(' + pos + ')');      
+
+        var linkText = document.createTextNode('Excluir')
+        linkElement.appendChild(linkText);
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
 
     }
 }
-Render()
+
+Render();
+
+function AddTodo() {
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value = '';
+    Render();
+    saveToStorage();
+
+}
+
+buttonElement.onclick = AddTodo;
+
+function deleteTodo() {
+    todos.splice(0, 1);
+    Render();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
